6ccfa809ca7a4aba6b40db1e3335b92c94cee8ef
Jeremy Baukens <jeremy.baukens@gmail.com>
2024-12-16 18:51:14 +0100

    Update player list to add action buttons

--- src/shared/TeamTargets/TeamTargets.js
+++ src/shared/TeamTargets/TeamTargets.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useCurrentTeam, useUniverseMenuData } from '../../context/PtreContext';
-import Tooltip from "./../../shared/Tooltip/Tooltip";
 
 import { formatDate } from '../../utils/date';
 import { formatWithThousandSeparator } from '../../utils/numbers';
@@ -14,6 +13,7 @@ const TeamTargets = () => {
 
     const [targets, setTargets] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [updatingRows, setUpdatingRows] = useState({});
 
     useEffect(() => {
         if (!teamData?.teamKey) {
@@ -36,7 +36,6 @@ const TeamTargets = () => {
                 );
 
                 if (response.RESULT_CODE === 0) {
-                    console.log(response)
                     setTargets(response.data.content);
                 } else {
                     console.log("API call failed. Please try again later.")
@@ -54,7 +53,38 @@ const TeamTargets = () => {
         };
 
         fetchTargets();
+        return () => controller.abort();
     }, [teamData?.teamKey, universeData?.community, universeData?.server]);
+
+    const handleAddToIGList = async (target, index, status) => {
+        setUpdatingRows((prev) => ({ ...prev, [index]: true }));
+
+        const teamKeydWithoutDash = teamData.teamKey.replace(/-/g, '');
+
+        try {
+            const response = await api.post(
+                `/api.php?view=target_update&country=${universeData.community}&univers=${universeData.server}&requested_status=${status}&player_id=${target.player_id}`,
+                { team_key: teamKeydWithoutDash }
+            );
+
+            if (response.RESULT_CODE === 0) {
+                setTargets((prevTargets) =>
+                    prevTargets.map((item, idx) =>
+                        idx === index
+                            ? { ...item, target_status: response.data.requested_status }
+                            : item
+                    )
+                );
+            } else {
+                console.log('Failed to update target status. Please try again later.');
+            }
+        } catch (error) {
+            console.error('Error updating target status:', error);
+        } finally {
+            setUpdatingRows((prev) => ({ ...prev, [index]: false }));
+        }
+    };
+
 
     const renderActivityColor = (lastActivity) => {
         if (!lastActivity) return 'gray';
@@ -84,7 +114,6 @@ const TeamTargets = () => {
         return 'red'; // > 1 month
     };
 
-
     return (
         <div className="container" style={{ marginTop: '10px' }}>
             <div className="box_1">
@@ -103,66 +132,77 @@ const TeamTargets = () => {
                                     <th>Alliance</th>
                                     <th>Fleet Points</th>
                                     <th>Ship Count</th>
-                                    <th>Last Spy
-                                        <Tooltip content={
-                                            <>
-                                                <p>Data from the last spy mission. This includes the most recent spy report of the target.</p>
-                                                <ul>
-                                                    <li><span style={{ color: 'green' }}>Green:</span> Recent check (&lt; 1 week)</li>
-                                                    <li><span style={{ color: 'orange' }}>Orange:</span> Moderate check (&lt; 1 month)</li>
-                                                    <li><span style={{ color: 'red' }}>Red:</span> No check for over a month</li>
-                                                    <li><span style={{ color: 'gray' }}>Gray:</span> No check recorded</li>
-                                                </ul>
-                                            </>
-                                        }>
-                                            <span className="tooltip-wrapper">
-                                                <span className="tooltip-icon">❓</span>
-                                            </span>
-                                        </Tooltip>
-                                    </th>
-                                    <th>Last Check
-                                        <Tooltip content={
-                                            <>
-                                                <p>Last time the player was checked.</p>
-                                                <ul>
-                                                    <li><span style={{ color: 'green' }}>Green:</span> Recent check (&lt; 1 hour)</li>
-                                                    <li><span style={{ color: 'orange' }}>Orange:</span> Moderate check (&lt; 1 day)</li>
-                                                    <li><span style={{ color: 'red' }}>Red:</span> No check for over a day</li>
-                                                    <li><span style={{ color: 'gray' }}>Gray:</span> No check recorded</li>
-                                                </ul>
-                                            </>
-                                        }>
-                                            <span className="tooltip-wrapper">
-                                                <span className="tooltip-icon">❓</span>
-                                            </span>
-                                        </Tooltip>
-                                    </th>
+                                    <th>Last Spy</th>
+                                    <th>Last Check</th>
                                     <th>IG Activities (Ally)</th>
+                                    <th className='text-center'>Status</th>
                                     <th>Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {targets.map((target, index) => (
                                     <tr key={index}>
-                                        <td>
-                                            {target.name}
-                                            {target.alias &&
-                                                target.alias !== target.name &&
-                                                ` (${target.alias})`}
-                                        </td>
+                                        <td>{target.name}</td>
                                         <td>{target.alliance || 'N/A'}</td>
                                         <td>{formatWithThousandSeparator(target.fleet_points)}</td>
                                         <td>{formatWithThousandSeparator(target.ship_count)}</td>
                                         <td style={{ color: renderLastSpyColor(target?.event_timestamp) }}>
                                             {target.event_timestamp ? formatDate(target.event_timestamp, 'long') : 'N/A'}
                                         </td>
-
                                         <td style={{ color: renderActivityColor(target.activity_last_event_timestamp) }}>
-                                            {target.activity_last_event_timestamp !== 0 ? formatDate(target.activity_last_event_timestamp, 'dateOnly') : 'N/A'}
+                                            {target.activity_last_event_timestamp !== 0
+                                                ? formatDate(target.activity_last_event_timestamp, 'dateOnly')
+                                                : 'N/A'}
                                         </td>
                                         <td>{target.activity_team_count} ({target.activity_coa_count})</td>
+                                        <td className='text-center'>
+                                            <span className={`target-current-status ${target.target_status === 0 ? 'enabled' : target.target_status === 1 ? 'ignored' : 'none'}`}>
+                                                {target.target_status === 0 ? 'ENABLED' : target.target_status === 1 ? 'IGNORED' : 'N/A'}
+                                            </span>
+                                        </td>
                                         <td>
-                                            <button className="btn-add-target">ADD TO IG LIST</button>
+                                            {updatingRows[index] ? (
+                                                'Processing...'
+                                            ) : (
+                                                <>
+                                                    {target.target_status === -1 && (
+                                                        <button
+                                                            className="btn-add-target"
+                                                            onClick={() => handleAddToIGList(target, index, '0')}
+                                                        >
+                                                            ADD TO IG LIST
+                                                        </button>
+                                                    )}
+                                                    {target.target_status === 0 && (
+                                                        <button
+                                                            className="btn-ignore-target"
+                                                            onClick={() => handleAddToIGList(target, index, '1')}
+                                                        >
+                                                            IGNORE
+                                                        </button>
+                                                    )}
+                                                    {target.target_status === 1 && (
+                                                        <div className="btn-group">
+                                                            <button
+                                                                className="btn-enable-target"
+                                                                onClick={() => handleAddToIGList(target, index, '0')}
+                                                            >
+                                                                ENABLE
+                                                            </button>
+                                                            <button
+                                                                className="btn-remove-target"
+                                                                onClick={(e) => {
+                                                                    e.stopPropagation();
+                                                                    handleAddToIGList(target, index, '-1');
+                                                                }}
+                                                                title="Remove from In-Game List"
+                                                            >
+                                                                <i className="fa fa-trash"></i>
+                                                            </button>
+                                                        </div>
+                                                    )}
+                                                </>
+                                            )}
                                         </td>
                                     </tr>
                                 ))}
